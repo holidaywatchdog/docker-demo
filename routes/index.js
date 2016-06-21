b8c6f2b0d9dd40293d7a51b5934ea4c9b94995c1
Robert Norman <rnorman@tripadvisor.com>
2016-06-21 13:26:37 +0100

    First commit for this demo application

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const Memcached = require('memcached');
+const router = express.Router();
+const _ = require('lodash');
+
+let memcached = new Memcached('127.0.0.1:11211');
+// const memcached = new Memcached(process.env.MEMCACHED_HOST);
+let title = process.env.PAGE_TITLE || 'Welcome to this homepage';
+
+const MC_COUNTER = 'counter';
+
+
+//Set default value;
+memcached.get(MC_COUNTER, function (err, counter) {
+
+  if(_.isUndefined(counter)){
+
+    memcached.set(MC_COUNTER, 0, 5000, function (err) {
+      console.log('SETTING STARTING VALUE');
+    });
+
+  }
+
+});
+
+router.get('/', function(req, res, next) {
+
+  memcached.get(MC_COUNTER, function (err, counter) {
+
+    console.log('Memcached counter ' + counter, err);
+
+    res.render('index', {
+      title: title,
+      counter: (counter ? counter++: 0)
+    });
+
+    memcached.incr(MC_COUNTER, 1, function (err) {
+      console.log('Incremented counter');
+    });
+
+  });
+
+});
+
+module.exports = router;
